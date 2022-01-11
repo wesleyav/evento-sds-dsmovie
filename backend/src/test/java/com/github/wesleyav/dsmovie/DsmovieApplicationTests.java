6766639639b2f51d6f3f58d4dd8a06ae34720ec7
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2022-01-10 23:02:18 -0300

    Project created

---
+++ backend/src/test/java/com/github/wesleyav/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
