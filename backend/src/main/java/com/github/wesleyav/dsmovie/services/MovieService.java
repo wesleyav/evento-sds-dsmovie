0865eb2b7acdac5a61663a8d7a2a4b20094b001c
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2022-01-12 19:40:13 -0300

    Find movies

---
+++ backend/src/main/java/com/github/wesleyav/dsmovie/services/MovieService.java
@@ -0,0 +1,33 @@
+package com.github.wesleyav.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.wesleyav.dsmovie.dto.MovieDTO;
+import com.github.wesleyav.dsmovie.entities.Movie;
+import com.github.wesleyav.dsmovie.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository movieRepository;
+
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Pageable pageable) {
+		Page<Movie> result = movieRepository.findAll(pageable);
+		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
+		return page;
+	}
+
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = movieRepository.findById(id).get();
+		MovieDTO dto = new MovieDTO(result);
+		return dto;
+	}
+
+}
