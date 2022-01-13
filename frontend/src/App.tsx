f2389b5d4a331280e5154aee07ff1c0ccd7d8b58
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2022-01-12 21:51:57 -0300

    Corrige nomenclatura de componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Listing from "components/pages/Listing";
-import Form from 'components/pages/Form';
+import Listing from "components/Pages/Listing";
+import Form from 'components/Pages/Form';
 import Navbar from "components/Navbar";
 
 function App() {
